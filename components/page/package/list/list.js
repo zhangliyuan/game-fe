07b7ac2304fbc054b0658753ed62dd83c7c780f2
zh.l.y <1529764427@qq.com>
2016-06-28 01:57:43 +0800

    1：礼包领取记录 2：礼包列表；

--- components/page/package/list/list.js
+++ components/page/package/list/list.js
@@ -5,10 +5,79 @@
 
 var $=require('/components/common/base/base.js');
 var Class=require('/components/common/class/class.js');
+var Ajax=require('/components/common/ajax/ajax.js');
+var Dialog = require('/components/common/dialog/dialog.js');
+var Filter = require('/components/widget/filter/filter.js');
+var Page       = require('/components/common/pager/pager.js');
+var PopTip = require('/components/common/pop-tip/pop-tip.js');
+var CollapseFilter = require('/components/widget/showFilter/collapse-filter.js');
 
 var PACKAGE_LIST = {
-    LAYOUT:__inline('list.tmpl')
+    LAYOUT:__inline('list.tmpl'),
+    PACKAGE_ITEM:__inline('package-item.tmpl'),
+    PACKAGE_DETAIL:__inline('package-detail.tmpl')
 };
+
+var FILTER_OPTIONS = [
+    {
+        label:'游戏名称',
+        name:'gameName',
+        type:'text',
+        placeholder:'',
+        validate: null
+    },
+    {
+        label:'礼包类型',
+        name:'type',
+        type:'select',
+        options:[
+            {text:'全部', value:'0'},
+            {text:'端游礼包', value:'1'},
+            {text:'手游礼包', value:'2'}
+        ],
+        placeholder:'',
+        validate: null
+    },
+    {
+        label:'礼包名称',
+        name:'packageName',
+        type:'text',
+        placeholder:'',
+        validate: null
+    },
+    {
+        label:'发放方式',
+        name:'distribute-type',
+        type:'select',
+        options:[
+            {text:'全部', value:'0'},
+            {text:'限制发放', value:'1'},
+            {text:'无限制发放', value:'2'}
+        ],
+        placeholder:'',
+        validate: null
+    },
+    {
+        label:'礼包编号',
+        name:'packageNo',
+        type:'text',
+        placeholder:'',
+        validate: null
+    },
+    {
+        label:'发布状态',
+        name:'status',
+        type:'select',
+        options:[
+            {text:'全部', value:'0'},
+            {text:'正在发放', value:'1'},
+            {text:'停止发放', value:'2'}
+        ],
+        placeholder:'',
+        validate: null
+    }
+];
+
 var PackageList = Class(function (opts) {
     this.opts = $.extend({
         container:'.admin-content'
@@ -19,12 +88,143 @@ var PackageList = Class(function (opts) {
         me.container = $(me.opts.container);
 
         me.render();
+        me.filter = new Filter({
+            container:'#filter-wrap',
+            renderData:FILTER_OPTIONS
+        });
+
+        me.getPackageList();
+
+        me.initEvents();
     },
 
     render: function () {
         var me = this;
         var _html = PACKAGE_LIST.LAYOUT({});
         me.container.empty().html(_html);
+    },
+
+    getPackageList: function (params) {
+        var me = this;
+
+        if(!params){
+            params = me.getFilterData();
+        }else{
+            params = $.extend(me.getFilterData(),params);
+        }
+
+        Ajax.get('/admin/package/list', params, function (data) {
+
+            //me.renderOrderList(data);
+            $('#package-list', me.container).empty().append(PACKAGE_LIST.PACKAGE_ITEM(data));
+
+            new Page({
+                target: '#package-list-page-wrap',
+                type: 'ajax',
+                pageCount: Number(data.total_count),
+                pageSize: 10,
+                callback: function(page){
+                    me.getPackageList({pageNo:page});
+                },
+                pn: Number(data.current_page)
+            });
+        });
+    },
+
+    initEvents: function () {
+        var me = this;
+
+        new CollapseFilter({
+            container:'.ico-collapse'
+        });
+
+        me.container.off('click');
+        me.container.on('click','#exec-search-btn', function () {
+            me.getPackageList();
+        });
+
+
+
+        me.container.on('click', '#package-list .operation', function () {
+            var obj = $(this);
+
+            var _operation = obj.data('oper');
+            var id = obj.closest('tr').data('id');
+            switch (_operation){
+                case 'detail':
+                    me.showPackageDetail(id);break;
+                case 'offShelf':
+                    me.changePackageStatus(id,'offShelf');break;
+                case 'edit':
+                    me.changePackageStatus(id,'edit');break;
+                case 'upload':
+                    me.changePackageStatus(id,status);break;
+                case 'delete':
+                    me.deletePackage(id);break;
+                default:
+                    console.log('do nothing');
+
+            }
+        });
+    },
+
+    showPackageDetail:function(id){
+        var me = this;
+
+
+        Ajax.get('/admin/package/detail',{id:id},function (data) {
+            var content = PACKAGE_LIST.PACKAGE_DETAIL(data);
+
+            Dialog.confirm(content, {
+                'width': '800px',
+                'height': 'auto',
+                'minHeight': 0,
+                'dialogClass': 'confirm-dialog',
+                'draggable': false,
+                'show': {
+                    'effect': 'drop',
+                    'mode': 'show',
+                    'direction': 'down',
+                    'duration': 100
+                },
+                'hide': {
+                    'effect': 'drop',
+                    'direction': 'down',
+                    'duration': 200
+                },
+
+                'buttons': [{
+                    'text': '确定',
+                    'className': 'btn btn-primary',
+                    'click': function(e) {
+                        e.preventDefault();
+                        e.stopImmediatePropagation();
+                        e.stopPropagation();
+
+                        $(this).dialog('close');
+                    }
+                }]
+            });
+
+        });
+    },
+    getFilterData: function () {
+        var me = this;
+
+        var filterData = me.filter.getFilterData();
+
+        return filterData;
+    },
+    changePackageStatus:function (id,status) {
+        Ajax.post('/admin/package/update',{id:id, status:status}, function (data) {
+            PopTip('操作成功！！');
+        });
+    },
+    deletePackage:function (id) {
+        // TODO 添加确认弹框
+        Ajax.post('/admin/package/delete',{id:id}, function (data) {
+            PopTip('操作成功！！');
+        });
     }
 });
 
--- components/page/package/records/list.js
+++ components/page/package/records/list.js
@@ -5,10 +5,78 @@
 
 var $=require('/components/common/base/base.js');
 var Class=require('/components/common/class/class.js');
+var Ajax=require('/components/common/ajax/ajax.js');
+var Dialog = require('/components/common/dialog/dialog.js');
+var Filter = require('/components/widget/filter/filter.js');
+var Tab = require('/components/common/tab/tab.js');
+var Page       = require('/components/common/pager/pager.js');
+var PopTip = require('/components/common/pop-tip/pop-tip.js');
+var CollapseFilter = require('/components/widget/showFilter/collapse-filter.js');
+
 
 var PACKAGE_RECORDS = {
-    LAYOUT:__inline('list.tmpl')
+    LAYOUT:__inline('list.tmpl'),
+    PACKAGE_RECORD_ITEM: __inline('package-record-item.tmpl'),
+    PACKAGE_RECORD_DETAIL: __inline('package-record-detail.tmpl')
 };
+
+
+var FILTER_OPTIONS = [
+    {
+        label:'游戏名称',
+        name:'gameName',
+        type:'text',
+        placeholder:'',
+        validate: null
+    },
+    {
+        label:'手机号',
+        name:'phoneNo',
+        type:'text',
+        placeholder:'',
+        validate: null
+    },
+    {
+        label:'礼包名称',
+        name:'packageName',
+        type:'text',
+        placeholder:'',
+        validate: null
+    },
+    {
+        label:'礼包编号',
+        name:'packageNo',
+        type:'text',
+        placeholder:'',
+        validate: null
+    },
+    {
+        label:'礼包CDK',
+        name:'packageCDK',
+        type:'text',
+        placeholder:'',
+        validate: null
+    },
+    {
+        label:'CDK状态',
+        name:'status',
+        type:'select',
+        options:[
+            {text:'全部', value:'0'},
+            {text:'已淘号', value:'1'},
+            {text:'未淘号', value:'2'}
+        ],
+        placeholder:'',
+        validate: null
+    }
+];
+
+var PACKAGE_STATUS = {
+    'normal': '正常',
+    'blocked': '封停',
+    'error': '异常'
+};
+
 var PackageRecords = Class(function (opts) {
     this.opts = $.extend({
         container:'.admin-content'
@@ -19,13 +87,128 @@ var PackageRecords = Class(function (opts) {
         me.container = $(me.opts.container);
 
         me.render();
+        me.filter = new Filter({
+            container:'#filter-wrap',
+            renderData:FILTER_OPTIONS
+        });
+
+        me.getPackageRecordList();
+
+        me.initEvents();
     },
 
     render: function () {
         var me = this;
         var _html = PACKAGE_RECORDS.LAYOUT({});
         me.container.empty().html(_html);
+    },
+
+    getPackageRecordList: function (params) {
+        var me = this;
+
+        if(!params){
+            params = me.getFilterData();
+        }else{
+            params = $.extend(me.getFilterData(),params);
+        }
+
+        Ajax.get('/admin/package_record/list', params, function (data) {
+
+            //me.renderOrderList(data);
+            $('#package-record-list', me.container).empty().append(PACKAGE_RECORDS.PACKAGE_RECORD_ITEM(data));
+
+            new Page({
+                target: '#package-record-list-page-wrap',
+                type: 'ajax',
+                pageCount: Number(data.total_count),
+                pageSize: 10,
+                callback: function(page){
+                    me.getPackageRecordList({pageNo:page});
+                },
+                pn: Number(data.current_page)
+            });
+        });
+    },
+
+    initEvents: function () {
+        var me = this;
+
+        new CollapseFilter({
+            container:'.ico-collapse'
+        });
+
+        me.container.off('click');
+        me.container.on('click','#exec-search-btn', function () {
+            me.getPackageRecordList();
+        });
+
+
+
+        me.container.on('click', '#package-record-list .operation', function () {
+            var obj = $(this);
+
+            var _operation = obj.data('oper');
+            var id = obj.closest('tr').data('id');
+            switch (_operation){
+                case 'detail':
+                    me.showPackageRecordDetail(id);break;
+                default:
+                    console.log('do nothing');
+
+            }
+        });
+    },
+
+    showPackageRecordDetail:function(id){
+        var me = this;
+
+
+        Ajax.get('/admin/package_record/detail',{id:id},function (data) {
+            var content = PACKAGE_RECORDS.PACKAGE_RECORD_DETAIL(data);
+
+            Dialog.confirm(content, {
+                'width': '800px',
+                'height': 'auto',
+                'minHeight': 0,
+                'dialogClass': 'confirm-dialog',
+                'draggable': false,
+                'show': {
+                    'effect': 'drop',
+                    'mode': 'show',
+                    'direction': 'down',
+                    'duration': 100
+                },
+                'hide': {
+                    'effect': 'drop',
+                    'direction': 'down',
+                    'duration': 200
+                },
+
+                'buttons': [{
+                    'text': '确定',
+                    'className': 'btn btn-primary',
+                    'click': function(e) {
+                        e.preventDefault();
+                        e.stopImmediatePropagation();
+                        e.stopPropagation();
+
+                        $(this).dialog('close');
+                    }
+                }]
+            });
+
+        });
+    },
+    getFilterData: function () {
+        var me = this;
+
+        var filterData = me.filter.getFilterData();
+
+        return filterData;
     }
+
+
+
 });
 
 module.exports=PackageRecords;
