ec64b6f4ab1796096bd0e7b9aad56573dc31b049
zh.l.y <1529764427@qq.com>
2016-07-09 17:38:56 +0800

    1，工具（邮件和短信）

--- components/page/tools/email/add.js
+++ components/page/tools/email/add.js
@@ -4,6 +4,8 @@
 
 var $=require('/components/common/base/base.js');
 var Class=require('/components/common/class/class.js');
+var Ajax=require('/components/common/ajax/ajax.js');
+var PopTip = require('/components/common/pop-tip/pop-tip.js');
 
 var ADD_EMAIL = {
     LAYOUT:__inline('add.tmpl')
@@ -18,12 +20,35 @@ var AddEmail = Class(function (opts) {
         me.container = $(me.opts.container);
 
         me.render();
+        me.initEvent();
     },
 
     render: function () {
         var me = this;
         var _html = ADD_EMAIL.LAYOUT({});
         me.container.empty().html(_html);
+    },
+
+    initEvent:function () {
+        var me = this;
+
+        me.container.off('click');
+
+        me.container.on('click', '#confirm-add-btn', function () {
+            var emailReceiver = me.container.find('#email-address').val();
+            var title = me.container.find('#email-title').val();
+            var content = me.container.find('#content').val();
+
+            Ajax.post('/admin/add_email', {
+                emailReceiver:emailReceiver,
+                title:title,
+                content:content
+            }, function (data) {
+                PopTip('发送成功！！！');
+                me.render();
+            });
+        });
+
     }
 });
 
--- components/page/tools/sms/add.js
+++ components/page/tools/sms/add.js
@@ -4,6 +4,8 @@
 
 var $=require('/components/common/base/base.js');
 var Class=require('/components/common/class/class.js');
+var Ajax=require('/components/common/ajax/ajax.js');
+var PopTip = require('/components/common/pop-tip/pop-tip.js');
 
 var ADD_SMS = {
     LAYOUT:__inline('add.tmpl')
@@ -18,12 +20,33 @@ var AddSMS = Class(function (opts) {
         me.container = $(me.opts.container);
 
         me.render();
+        me.initEvent();
     },
 
     render: function () {
         var me = this;
         var _html = ADD_SMS.LAYOUT({});
         me.container.empty().html(_html);
+    },
+
+    initEvent:function () {
+        var me = this;
+
+        me.container.off('click');
+
+        me.container.on('click', '#confirm-add-btn', function () {
+            var phoneNo = me.container.find('#phoneNo').val();
+            var content = me.container.find('#content').val();
+
+            Ajax.post('/admin/add_sms', {
+                phoneNo:phoneNo,
+                content:content
+            }, function (data) {
+                PopTip('发送成功！！！');
+                me.render();
+            });
+        });
+
     }
 });
 
